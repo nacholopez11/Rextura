6fb3f8485b3303585b50f3453a8cbd4603f19df6
nacholopez11 <nacho.lopez.esmc@gmail.com>
2024-02-07 16:42:37 +0100

    ya suma y resta el total en el pedido pero en la bd no inserta el precio con descuento

--- assets/js/usarPuntos.js
+++ assets/js/usarPuntos.js
@@ -4,42 +4,42 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let totalPedidoElement = document.getElementById('totalPedido');
     let usuarioId = document.getElementById('usuarioId').value;
 
-    // Obtiene los puntos del usuario
-    fetch('https://localhost/rextura/index.php?controller=api&action=api&accion=obtenerPuntos', {
-        method: 'POST',
-        body: JSON.stringify({
-            usuario_id: usuarioId,
-        }),
-        headers: {
-            'Content-Type': 'application/json;',
-        },
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Error de red');
-        }
-        return response.json();
-    })
-    .then(data => {
-        let puntos = data.puntos;
+    // Almacena el total original del pedido
+    let totalOriginal = parseFloat(totalPedidoElement.innerText.replace('€', ''));
 
-        // Añade un controlador de eventos al checkbox
-        checkboxUsarPuntos.addEventListener('change', function() {
-            // Obtiene el total del pedido
-            let totalPedido = parseFloat(totalPedidoElement.innerText.replace('€', ''));
+    // Añade un controlador de eventos al checkbox
+    checkboxUsarPuntos.addEventListener('change', function() {
+        // Obtiene los puntos del usuario
+        fetch('https://localhost/rextura/index.php?controller=api&action=api&accion=obtenerPuntos', {
+            method: 'POST',
+            body: JSON.stringify({
+                usuario_id: usuarioId,
+            }),
+            headers: {
+                'Content-Type': 'application/json;',
+            },
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error de red');
+            }
+            return response.json();
+        })
+        .then(data => {
+            let puntos = data.puntos;
 
             // Calcula el descuento
-            let descuento = this.checked ? Math.min(totalPedido, puntos) : 0;
+            let descuento = this.checked ? Math.min(totalOriginal, puntos) : 0;
 
             // Calcula el nuevo total
-            let nuevoTotal = totalPedido - descuento;
+            let nuevoTotal = totalOriginal - descuento;
 
             // Actualiza el total del pedido en la página
             totalPedidoElement.innerText = nuevoTotal.toFixed(2) + ' €';
+        })
+        .catch(error => {
+            console.log(error);
+            notie.alert({ type: 'error', text: 'Error al obtener los puntos de fidelidad', time: 2 });
         });
-    })
-    .catch(error => {
-        console.log(error);
-        notie.alert({ type: 'error', text: 'Error al obtener los puntos de fidelidad', time: 2 });
     });
 });
